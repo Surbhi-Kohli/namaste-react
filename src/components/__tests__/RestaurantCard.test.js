afb93b930d37f5a32d3ac8e5c644e8a45051807d
Surbhi-Kohli <surbhi.kohli@walmart.com>
2023-10-08 23:38:59 +0530

    added HOC testing

--- src/components/__tests__/RestaurantCard.test.js
+++ src/components/__tests__/RestaurantCard.test.js
@@ -1,16 +1,20 @@
-import { render, screen } from "@testing-library/react";
+import { render , screen} from "@testing-library/react";
+import "@testing-library/jest-dom";//to use toBeInTheDocument
 import RestaurantCard from "../RestaurantCard";
-import MOCK_DATA from "../mocks/resCardMock.json";
-import "@testing-library/jest-dom";
-
-it("should render RestaurantCard component with props Data", () => {
-  render(<RestaurantCard resData={MOCK_DATA} />);
-
-  const name = screen.getByText("Leon's - Burgers & Wings (Leon Grill)");
-
+import { withPromtedLabel } from "../RestaurantCard" 
+import MOCK_DATA from "../mocks/resCardMock.json"
+it("should render RestaurantCard component with props Data",()=>{
+  render(<RestaurantCard resData={MOCK_DATA}/>);
+  const name=screen.getByText("Leon's - Burgers & Wings (Leon Grill)");
   expect(name).toBeInTheDocument();
-});
+})
+it("should render restaurantcard component with promoted label",()=>{
+    //test HOC:withPromotedLabel
+    const RenderHOC = withPromtedLabel(RestaurantCard);
+   render(<RenderHOC resData={MOCK_DATA}/>);
+   const label=screen.getByText("Promoted");
+    const restCard=screen.getByTestId("resCard");
+     expect(label).toBeInTheDocument();
+     expect(restCard).toBeInTheDocument();
 
-it("should render RestaurantCard component with Promoted Label", () => {
-  // Home Work - test HOC : withPromtedLabel()
-});
+})
